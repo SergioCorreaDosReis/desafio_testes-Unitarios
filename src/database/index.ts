53451960b10833e4dfbb016c5483b24d08aa34c3
Sergio Reis <sergioc_reis@hotmail.com>
2021-07-08 17:55:15 -0300

    generate docker image of fin_api and fin_api_db both are running well

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,16 @@
-import { createConnection } from 'typeorm';
+import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
-(async () => await createConnection())();
+export default async (host = "database"): Promise<Connection> => {
+	const defaultOptions = await getConnectionOptions();
+
+	return createConnection(
+		Object.assign(defaultOptions, {
+			host: process.env.NODE_ENV === "test" ? "localhost" : host,
+			// Verifica se o conteudo da variavel NOD_ENV do package.json esta com apontando para test se sim usa o banco referenciado senao usa o default
+			database:
+				process.env.NODE_ENV === "test"
+					? "fin_api_test"
+					: defaultOptions.database,
+		})
+	);
+};
