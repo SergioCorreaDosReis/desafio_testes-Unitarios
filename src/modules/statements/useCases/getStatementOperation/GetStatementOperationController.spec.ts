865d78a859678ac72860e3ad61104f33531b45f0
Sergio Reis <sergioc_reis@hotmail.com>
2021-07-10 18:41:29 -0300

    add integration test to return information about expecific statement operation by statement id

---
+++ src/modules/statements/useCases/getStatementOperation/GetStatementOperationController.spec.ts
@@ -0,0 +1,77 @@
+import { hash } from "bcryptjs";
+import request from "supertest";
+import { Connection } from "typeorm";
+import { v4 as uuidv4 } from "uuid";
+
+import { app } from "../../../../app";
+import createConnection from "@database/index";
+import { ICreateStatementDTO } from "../createStatement/ICreateStatementDTO";
+
+let connection: Connection;
+
+describe("Return statement operation", () => {
+  beforeAll(async () => {
+    connection = await createConnection();
+    // antes de criar as conexões executar as migrations
+    await connection.runMigrations();
+
+    const id = uuidv4();
+    const password = await hash("admin", 8);
+
+    await connection.query(
+      `INSERT INTO USERS (id, name, email, password, created_at, updated_at)
+		VALUES ('${id}','admin4','admin4@admin.com','${password}', 'now()', 'now()')
+			 `
+    );
+  });
+
+  it("should be able to return a information of a statement operation by statement id", async () => {
+    const authenticateUser = await request(app).post("/api/v1/sessions").send({
+      email: "admin4@admin.com",
+      password: "admin",
+    });
+
+    const { id, token } = authenticateUser.body;
+
+    enum OperationType {
+      DEPOSIT = "deposit",
+      WITHDRAW = "withdraw",
+    }
+
+    // Criar a variavel deposito
+    const deposit: ICreateStatementDTO = {
+      user_id: id,
+      description: "deposit test",
+      amount: 500,
+      type: "deposit" as OperationType,
+    };
+
+    const statement = await request(app)
+      .post("/api/v1/statements/deposit")
+      .send(deposit)
+      .set({ Authorization: `Bear ${token}` });
+
+    // console.log(statement.body)
+
+    const response = await request(app)
+      .get(`/api/v1/statements/${statement.body.id}`)
+      .set({ Authorization: `Bear ${token}` });
+
+    await expect(response.status).toBe(200);
+    await expect(response.body.id).toEqual(statement.body.id);
+  });
+
+  it("should not be able to return a information to non authenticated user", async () => {
+      const response = await request(app)
+      .get(`/api/v1/statements/${"123"}`)
+      .set({ Authorization: `Bear ${123}` });
+
+      await expect(response.status).toBe(401);
+    await expect(response.body.message).toEqual("JWT invalid token!");
+  });
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  });
+});
